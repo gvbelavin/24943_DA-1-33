a4eefd4c4cfe89083112ca8effe2efc971f24d69
gvbelavin <97274686+gvbelavin@users.noreply.github.com>
2025-09-23 17:42:26 +0700

    Update DA-1-33.py

--- DA-1-33.py
+++ DA-1-33.py
@@ -1,9 +1,36 @@
 import seaborn as sns
 from sklearn.datasets import load_diabetes
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy
 
-diabetes = load_diabetes()
-df = pd.DataFrame(data=diabetes.data,
-                           columns=diabetes.feature_names)
-df['Progress'] = diabetes.target
-sns.kdeplot(data=df, x='Progress', color='green', fill=True)
+def diabet_kde(dataFrame=None,target_column = "Progress",color = "green",save_path=None):
+  if dataFrame is None:
+    diabetes = load_diabetes()
+    dataFrame = pd.DataFrame(data=diabetes.data,
+                              columns=diabetes.feature_names)
+    dataFrame['Progress'] = diabetes.target
+
+  if target_column not in dataFrame.columns:
+    avalible_columns = ','.join(dataFrame.columns)
+    raise ValueError(f"Колонка {target_column} не найдена в dataFrame")
+    print(f"Доступные колонки {avalible_columns}")
+
+
+  sns.kdeplot(data=dataFrame, x=target_column, color= color, fill=True)
+
+  plt.title("Ядерная оценка плотности распределения прогресса диабета", fontsize=14)
+  plt.xlabel("Уровень прогресса диабета",fontsize=12)
+  plt.ylabel("Плотность вероятности",fontsize=12)
+  sns.set_style("whitegrid")
+  plt.show()
+
+  if save_path:
+        # Укажите полный путь и имя файла
+    plt.savefig(save_path)
+
+if __name__ == "__main__":
+  try:
+    diabet_kde()
+  except Exception as error:
+    print(f"Произошла ошибка {error}")
